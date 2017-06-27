66b16616ad3b943e91f7eb1f281a44530035e942
BigBeardy <westderick70@gmail.com>
2017-06-27 10:17:05 -0400

    addition adjusted

--- adding.rb
+++ adding.rb
@@ -1,6 +1,13 @@
+# puts "please enter a number"
+
+def addition(num_1,num_2)
+    
+    puts "#{num_1}+#{num_2} = #{num_1 + num_2}" 
+end 
 puts "please enter a number"
 num_1 = gets.chomp.to_i
 puts "please give second number"
 num_2 =gets.chomp.to_i  
-num_3 = num_1 + num_2
-puts "#{num_1}+#{num_2}=#{num_3}" 
+# num_3 = num_1 + num_2
+# puts "#{num_1}+#{num_2}=#{num_3}" 
+ addition(num_1,num_2)
